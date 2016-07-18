750a5e57e862ce2952c41774778c6be6c183be83
eveningCoder <safaldasg@gmail.com>
2016-07-18 15:22:49 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require("path");
+
+module.exports = {
+  entry : "app.js",
+  output : {
+    path:path.resolve('./dist'),
+    filename:"app.js",
+    sourceMapFile:"app.map"
+  },
+  devtool:'#source-map',
+  module:{
+    loaders:[
+    {
+      loader:'babel-loader',
+      test:/\.js$/,
+      exclude: /node_modules/,
+      query:{
+        presets:['es2015']
+      }
+    }
+    ]
+  },
+  resolve:{
+   root:path.resolve('./src'),
+   extensions:['.jsx','.js']
+  }
+}
